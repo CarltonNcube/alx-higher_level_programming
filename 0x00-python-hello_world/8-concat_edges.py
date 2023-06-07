ed2ef707533fe873cbc60341584bfb4b46708fba
CarltonNcube <carltonncube@outlook.com>
2023-06-07 14:01:42 -0700

    Copy Cut Paste

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = s[32:46] + s[35:38] + s[0:5] + s[47:57]
+print(str)
