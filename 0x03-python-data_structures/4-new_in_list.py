f26a5623550e2565d7b3feb29c3d27b82c82d0cc
CarltonNcube <carltonncube@outlook.com>
2023-06-12 22:09:50 +0000

    replaces an element in a list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return list(my_list)
+    else:
+        new_list = list(my_list)
+        new_list[idx] = element
+        return new_list
+
+my_list = list(range(1, 6))
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
