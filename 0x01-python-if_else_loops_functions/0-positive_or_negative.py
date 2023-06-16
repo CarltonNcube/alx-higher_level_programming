13e1232e4f7dd92d88e3cd5c4bf3a4ae4dfe09a5
CarltonNcube <carltonncube@outlook.com>
2023-06-16 01:55:27 +0000

    print whether the number stored in the variable number is positive or negative

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-if number > 0:
+
+# if number is greater than 0 and less than or equal to 10, print "is positive"
+if number in range(1, 11):
     print("is positive")
-elif number <  0:
+# elif number is less than 0 and greater than or equal to -10, print "is negative"
+elif number in range(-10, 0):
     print("is negative")
-else:
+# else if number is 0, print "is zero"
+elif number == 0:
     print("is zero")
+else:
+    print("Out of range")
