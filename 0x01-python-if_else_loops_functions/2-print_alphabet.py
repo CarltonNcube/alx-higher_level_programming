57ebfdcb3971850cd4971c7e494b8ee5afa5d03c
CarltonNcube <carltonncube@outlook.com>
2023-06-16 21:29:59 +0000

    a program that prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 #Print the lowercase letters from 'a' to 'z' 
-for ascii_value in range(97, 123):  
-    print(chr(ascii_value), end='')
+alphabet = "" # create an empty string variable
+for i in range(97, 123): # loop from 97 to 122, which are the ASCII codes for 'a' to 'z'
+    alphabet = "{}{}".format(alphabet, chr(i)) # use the format() method to insert the character corresponding to the ASCII code into the string variable
+print(alphabet)
