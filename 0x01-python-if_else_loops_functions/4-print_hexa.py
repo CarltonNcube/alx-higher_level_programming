36fe76038562b36692b6d27c8deb011f2e6aeeda
CarltonNcube <carltonncube@outlook.com>
2023-06-17 13:09:50 +0000

    prints all numbers from 0 to 98 in decimal and in hexadecimal

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#for loop that will iterate over the numbers 0 to 98 (99 numbers in total)
+#i will take on each of these values in sequence.
+#it uses string format is used to define the desired output format
+for i in range(99):
+    print("{} = 0x{:02x}".format(i, i))
