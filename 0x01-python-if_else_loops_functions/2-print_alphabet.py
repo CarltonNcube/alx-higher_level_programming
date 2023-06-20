ccb3e24c37e618cf5c387103ebd23e9e714972ea
CarltonNcube <carltonncube@outlook.com>
2023-06-20 18:44:04 +0000

    prints the ASCII alphabet, in lowercase, not followed by a new line

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-#Print the lowercase letters from 'a' to 'z' 
-alphabet = "" # create an empty string variable
-for i in range(97, 123): # loop from 97 to 122, which are the ASCII codes for 'a' to 'z'
-    print("{}{}".format(alphabet, chr(i)), end="") # use the format() method to insert the character corresponding to the ASCII code into the string variable
+for i in range(ord('a'), ord('z')+1):
+    print(chr(i), end='')
 
+print()
