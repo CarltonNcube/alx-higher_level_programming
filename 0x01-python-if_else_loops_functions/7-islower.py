bdd5d38c14bc0a59dd251169a9b03aa6b3bfe546
CarltonNcube <carltonncube@outlook.com>
2023-06-21 17:48:00 +0000

    a function that checks for lowercase character

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-#This code uses ord() function to get the ASCII value of the character.
-#Lowercase letters in ASCII range from 97 to 122
-#while uppercase letters range from 65 to 90
-#By comparing the ASCII value of the character c with these ranges, you can determine if it is lowercase or uppercase.
+# This code uses ord() function to get the ASCII value of the character.
+# Lowercase letters in ASCII range from 97 to 122
+# while uppercase letters range from 65 to 90
+# By comparing the ASCII value of the character c with these ranges, you can determine if it is lowercase or uppercase.
 
 def islower(c):
     if ord('a') <= ord(c) <= ord('z'):
-       # print(c, "is lowercase")
         return True
     elif ord('A') <= ord(c) <= ord('Z'):
-       # print(c, "is uppercase")
         return False
     else:
        pass
