05cbc66944d8d553b221e49a1e5ecf2461b2a34a
CarltonNcube <carltonncube@outlook.com>
2023-06-24 19:22:39 +0000

    a function that retrieves an element from a list like in C

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-# Call the function with the numbers list
-
 def print_list_integer(my_list=[]):
-    for number in my_list:
-        print("{:d}".format(number))
+    for item in my_list:
+        if isinstance(item, int):
+            print("Integer: {}".format(item))
+
+my_list = [1, 2, 3, 4, 5]
+print_list_integer(my_list)
