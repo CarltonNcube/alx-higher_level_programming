9f340820869f874ec7f4c1e7bbaa7baebf4964eb
CarltonNcube <carltonncube@outlook.com>
2023-06-25 07:20:50 +0000

    a program that imports the function

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+fromm add_0.py import add
+
+# Assigning values to variables a and b
+a = 1
+b = 2
+
+# Calling the add function and assigning the result to a variable called result
+result = add(a, b)
+
+# Printing the result using string formatting
+print("{} + {} = {}".format(a, b, result))
+
