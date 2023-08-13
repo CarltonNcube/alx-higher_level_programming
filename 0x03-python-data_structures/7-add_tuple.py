e45b0a61069f5d0854a0dd94058bd9fd879375fd
CarltonNcube <carltonncube@outlook.com>
2023-08-13 01:19:03 -0700

    a function that adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) != 2 or len(tuple_b) != 2:
-        raise ValueError("Both tuples must contain exactly 2 elements.")
-    
-    sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return sum_tuple
+
+    while len(tuple_a) < 2:
+        tuple_a += (0,)
+    while len(tuple_b) < 2:
+        tuple_b += (0,)
+
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
