de73f90807317245bad248838d19b2563de9eed0
CarltonNcube <carltonncube@outlook.com>
2023-08-14 13:02:42 -0700

    a program that imports all functions from the file calculator_1.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,7 +7,7 @@ def main():
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
@@ -23,7 +23,7 @@ def main():
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
+
     print(f"{a} {operator} {b} = {result}")
 
 if __name__ == "__main__":
