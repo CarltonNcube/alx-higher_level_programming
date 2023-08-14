0b5bbd8064193e05b915bfe3b7f9ad786f9fd216
CarltonNcube <carltonncube@outlook.com>
2023-08-14 12:12:11 -0700

    a program that imports functions from the file

---
+++ 0x02-python-import_modules/main_script.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from calculation_1 import add, sub, mul, div
+
+result = add(10, 5)
+print(result)
+
+result = sub(10, 5)
+print(result)
+
+result = mul(10, 5)
+print(result)
+
+result = div(10, 5)
+print(result)
