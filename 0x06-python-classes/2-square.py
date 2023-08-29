2ebc30a451dd7ebaea2d4d32ad98b2951cbd08d2
CarltonNcube <carltonncube@outlook.com>
2023-08-29 23:26:57 +0000

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,25 +2,62 @@
 
 class Square:
     """
-    A class representing a square.
+    A class that defines a square.
 
     Attributes:
-        __size (int): The size of the square.
+        __size (int): Private attribute for the size of the square.
     """
 
     def __init__(self, size=0):
         """
-        Initializes a Square instance.
+        Initializes a new Square instance.
 
         Args:
-            size (int, optional): The size of the square. Defaults to 0.
-        
-        Raises:
-            TypeError: If size is not an integer.
-            ValueError: If size is less than 0.
+            size (int, optional): The size of the square. Default is 0.
         """
+        # Checking if the provided size is valid
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+
+        if size < 0:
             raise ValueError("size must be >= 0")
+
         self.__size = size
+
+# Creating an instance of the Square class with size 3
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+# Creating an instance of the Square class with default size
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+# Trying to access the size attribute directly
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+# Trying to access the __size attribute directly
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+# Trying to create a Square instance with a non-integer size
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+# Trying to create a Square instance with a negative size
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
