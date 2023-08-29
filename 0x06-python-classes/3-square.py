55fed90d7e6a62d925d1e330f9e98fb3768e272e
CarltonNcube <carltonncube@outlook.com>
2023-08-29 23:22:50 +0000

    Write a class Square that defines a square by: (based on 2-square.py)

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,10 +6,6 @@ class Square:
 
     Attributes:
         __size (int): Private attribute for the size of the square.
-
-    Methods:
-        __init__(self, size=0): Initializes a new Square instance with the given size.
-        area(self): Calculates and returns the current area of the square.
     """
 
     def __init__(self, size=0):
@@ -19,18 +15,38 @@ class Square:
         Args:
             size (int, optional): The size of the square. Default is 0.
         """
+        self.__size = size
+
+        # Checking if the provided size is valid
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
+
         if size < 0:
             raise ValueError("size must be >= 0")
-        
-        self.__size = size
-    
+
     def area(self):
         """
-        Calculates and returns the current area of the square.
-
-        Returns:
-            int: The area of the square.
+        Returns the area of the square.
         """
         return self.__size ** 2
+
+# Creating an instance of the Square class with size 3
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
+
+# Trying to access the size attribute directly
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+# Trying to access the __size attribute directly
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+# Creating an instance of the Square class with size 5
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
+
