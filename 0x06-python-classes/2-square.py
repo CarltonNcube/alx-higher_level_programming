aa789eaffe5e5bd6910d11c9b207adf1491ea2b7
CarltonNcube <carltonncube@outlook.com>
2023-08-29 23:31:31 +0000

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -23,3 +23,9 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
+    def area(self):
+        """
+        Returns the area of the square.
+        """
+        return self.__size ** 2
