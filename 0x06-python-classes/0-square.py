7841b19824b45ecf12220475141b3b35ca8279e8
CarltonNcube <carltonncube@outlook.com>
2023-08-30 00:08:28 +0000

    an empty class Square that defines a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 
 class Square:
-    """
-    A class that defines a square.
-    """
+    """A class that defines a square."""
     pass
-
-my_square = Square()
-
-# Printing the type and dictionary of the square object
-print(type(my_square))
-print(my_square.__dict__)
