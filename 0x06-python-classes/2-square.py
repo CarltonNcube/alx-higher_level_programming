1138324ebc4d24fd0802879e1af0efcc166578f7
CarltonNcube <carltonncube@outlook.com>
2023-08-30 00:24:18 +0000

    a class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -11,22 +11,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
-# Example usage:
-if __name__ == "__main__":
-    # Create a Square instance with size 5
-    square = Square(5)
-    print("Size:", square._Square__size)
-    
-    # Try to create a Square instance with a non-integer size
-    try:
-        invalid_square = Square("hello")
-    except TypeError as e:
-        print("Error:", e)
-    
-    # Try to create a Square instance with a negative size
-    try:
-        invalid_square = Square(-2)
-    except ValueError as e:
-        print("Error:", e)
-
