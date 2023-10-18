156a8c05ab035237793121e9d3565dc4b7d3e681
CarltonNcube <carltonncube@outlook.com>
2023-10-18 14:59:45 -0700

    a script that prints the full description of the table first_table from the database hbtn_0c_0

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 -- SQL query to show the create table statement for 'first_table'
-SHOW CREATE TABLE 'first_table';
+SELECT column_name, column_type, is_nullable, column_key, column_default, extra
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
