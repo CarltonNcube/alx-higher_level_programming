06d88e03e1eab7c2366027c981a5b5aefe7b8a52
CarltonNcube <carltonncube@outlook.com>
2023-10-18 13:35:34 -0700

    a script that creates the database hbtn_0d_2 and the user user_0d_2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- Create MySQL user user_0d_2 with SELECT privilege on database hbtn_0d_2
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
 -- Grant SELECT privilege to user_0d_2 on database hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
