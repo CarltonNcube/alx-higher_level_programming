36488b5c87ce9b3f988d3fb03090c9a68ebb2c82
CarltonNcube <carltonncube@outlook.com>
2023-11-14 13:04:32 -0800

    a class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// A class Rectangle that defines a rectangle with a constructor that
+// validates the arguments
+
+class Rectangle {
+  // The constructor takes two arguments w and h
+  constructor(w, h) {
+    // If w or h is equal to 0 or not a positive integer, create an empty object
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return;
+    }
+    // Otherwise, initialize the instance attribute width with the value of w
+    this.width = w;
+    // And initialize the instance attribute height with the value of h
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
