94186d0c3add849c31ed514c732c291ac778c487
CarltonNcube <carltonncube@outlook.com>
2023-11-14 14:01:47 -0800

    a script that imports an array and computes a new array

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// import list from the file 100-data.js
+const list = require('./100-data').list;
+// print the initial list
+console.log(list);
+// use map to create a new list with each value multiplied by the index
+const newList = list.map((value, index) => value * index);
+// print the new list
+console.log(newList);
