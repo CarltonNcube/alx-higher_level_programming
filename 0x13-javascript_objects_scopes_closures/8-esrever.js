82ed878dd501e09dccb4d6e17b9aa74563707456
CarltonNcube <carltonncube@outlook.com>
2023-11-14 13:50:21 -0800

    a function that returns the reversed version of a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  // initialize an empty array
+  let reversed = [];
+  // loop through the list from the end to the beginning
+  for (let i = list.length - 1; i >= 0; i--) {
+    // push the current element to the reversed array
+    reversed.push(list[i]);
+  }
+  // return the reversed array
+  return reversed;
+};
