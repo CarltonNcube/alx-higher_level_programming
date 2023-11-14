a8113bf1985bea3894e1264bf04f5ce8a377d0be
CarltonNcube <carltonncube@outlook.com>
2023-11-13 17:34:55 -0800

    a script that prints 3 lines

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-let firstLine = 'C is fun';
-let secondLine = 'Python is cool';
-let thirdLine = 'JavaScript is amazing';
-console.log(firstLine);
-console.log(secondLine);
-console.log(thirdLine);
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const line of lines) {
+  console.log(line);
+}
