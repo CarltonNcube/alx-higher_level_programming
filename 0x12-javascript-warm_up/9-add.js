5764252d2a767166b60dc057d15a466217f87393
CarltonNcube <carltonncube@outlook.com>
2023-11-13 17:30:14 -0800

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
 }
 
-const arg1 = parseInt(process.argv[2]);
-const arg2 = parseInt(process.argv[3]);
-
-if (isNaN(arg1) || isNaN(arg2)) {
-  console.log('Arguments must be integers');
-} else {
-  console.log(add(arg1, arg2));
-}
+add(Number(process.argv[2]), Number(process.argv[3]));
