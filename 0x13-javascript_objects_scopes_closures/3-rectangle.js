df1aff6c13d274c9d7928cc8e129d534f3f3763f
CarltonNcube <carltonncube@outlook.com>
2023-11-15 18:15:12 -0800

    a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     // check if w and h are positive integers
     if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
@@ -13,7 +13,7 @@ class Rectangle {
     }
   }
 
-  print() {
+  print () {
     // print the rectangle using X
     if (this.width > 0 && this.height > 0) {
       for (let i = 0; i < this.height; i++) {
