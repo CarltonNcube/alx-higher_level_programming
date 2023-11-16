d94c86ab1db3b61bd9aee5b8e94c3d7caef9ad1e
CarltonNcube <carltonncube@outlook.com>
2023-11-15 18:30:25 -0800

    a class Square that defines a square and inherits from Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 
+// Import the SquareP class from the specified module ('./5-square')
 const SquareP = require('./5-square');
 
+// Define a Square class that extends the SquareP class
 class Square extends SquareP {
+  // Define a method for printing the square with a specified character
   charPrint(c) {
     // If c is undefined, use 'X'
     c = c || 'X';
 
     // Validate that c is a single character
     if (typeof c !== 'string' || c.length !== 1) {
+      // Throw an error if the provided character is invalid
       throw new Error('Invalid character. Please provide a single character.');
     }
 
@@ -19,4 +23,5 @@ class Square extends SquareP {
   }
 }
 
+// Export the Square class to make it available for use in other files/modules
 module.exports = Square;
