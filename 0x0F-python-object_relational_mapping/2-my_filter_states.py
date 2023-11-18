6dbaa44310b18e639aa91b0cd1f039e6df3853bb
CarltonNcube <carltonncube@outlook.com>
2023-11-18 12:55:44 -0800

    a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -3,36 +3,24 @@
 import sys
 import MySQLdb
 
-if __name__ == '__main__':
-    # Check if the correct number of command-line arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name".format(sys.argv[0]))
-        exit(1)
+if __name__ == "__main__":
+    try:
+        # Connect to the MySQL database
+        with MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+                db=sys.argv[3], port=3306) as db:
+            # Create a cursor object to interact with the database
+            with db.cursor() as cur:
+                # Execute the SQL query with a parameterized query
+                cur.execute("SELECT * FROM states WHERE name LIKE BINARY %s", (sys.argv[4],))
 
-    # Connect to the MySQL database
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
-    )
+                # Fetch all rows from the result set
+                query_rows = cur.fetchall()
 
-    # Create a cursor object to interact with the database
-    cur = conn.cursor()
+                # Print each row (state) in the result set
+                for row in query_rows:
+                    print(row)
 
-    # Execute the SQL query to select states with a specific name (case-sensitive) and order them by ID
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' \
-                ORDER BY id ASC".format(sys.argv[4]))
+    except MySQLdb.Error as e:
+        print(f"Database Error: {e}")
+        sys.exit(1)
 
-    # Fetch all rows from the result set
-    query_rows = cur.fetchall()
-
-    # Print each row (state) in the result set
-    for row in query_rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cur.close()
-    conn.close()
