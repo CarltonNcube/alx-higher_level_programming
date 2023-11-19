e170243ed96b52e23fe9ea94e0136f5685627b63
CarltonNcube <carltonncube@outlook.com>
2023-11-19 08:37:57 -0800

    a script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as an argument from the database hbtn_0e_6_usa
+Prints the State object with the name passed as an argument from the database
 """
 
 import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
     # Check if correct number of command-line arguments is provided
     if len(sys.argv) != 5:
         print("Usage: {} <mysql_username> <mysql_password> <database_name> <state_name>".format(sys.argv[0]))
-            sys.exit(1)
+        sys.exit(1)
 
     # Create a SQLAlchemy engine to connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(sys.argv[1],
+        sys.argv[2], sys.argv[3]))
+
+    # Create the table in the MySQL database based on the defined State class
+    Base.metadata.create_all(engine)
 
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
@@ -26,12 +29,13 @@ if __name__ == "__main__":
 
     # Search for the State object with the given name
     state_name_to_search = sys.argv[4]
-    found_state = session.query(State).filter_by(name=state_name_to_search).first()
+    instance = session.query(State).filter(State.name == state_name_to_search).all()
 
-    # Display the result or "Not found" if no state with the given name is found
-    if found_state:
-        print(found_state.id)
-    else:
+    try:
+        # Print the id of the found State object
+        print(instance[0].id)
+    except IndexError:
+        # If no state with the given name is found, print "Not found"
         print("Not found")
 
     # Close the session
