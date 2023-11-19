c60c40575a1ef43ea87c9032676b008a217765ab
CarltonNcube <carltonncube@outlook.com>
2023-11-19 00:21:26 -0800

    a script that lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 """
-Lists all cities from the database hbtn_0e_4_usa.
+Lists all cities with their corresponding state names from the database hbtn_0e_0_usa.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if the correct number of command-line arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
-        sys.exit(1)
-
     # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
-
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
+            db=sys.argv[3], port=3306)
+    
     # Create a cursor object to interact with the database
     cur = db.cursor()
 
-    # Execute the SQL query to select all cities and order them by ID
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    # Execute the SQL query to select cities with their corresponding state names
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
 
     # Fetch all rows from the result set
     rows = cur.fetchall()
 
-    # Print each row (city) in the result set
+    # Print each row (city and state) in the result set
     for row in rows:
         print(row)
 
