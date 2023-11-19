0a9d8ce4a32494a783e9fda7cf86028d07d57223
CarltonNcube <carltonncube@outlook.com>
2023-11-19 07:37:18 -0800

    a python file that contains the class definition of a State and an instance Base = declarative_base()

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
 """
-Start link class to table in the database
+Start link class to table in database
 """
 
 import sys
-# Importing the Base and State classes from the model_state module
 from model_state import Base, State
-
-# Importing the create_engine function from the SQLAlchemy module
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    # Print debug information
-    print("Command-line arguments:", sys.argv)
-
-    # Creating a SQLAlchemy engine to connect to the MySQL server
+    # Create a SQLAlchemy engine to connect to the MySQL server
+    # Use command-line arguments for MySQL username, password, and database name
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
         sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-
-    # Print debug information
-    print("SQLAlchemy Engine URL:", engine.url)
 
     # Create the table in the MySQL database based on the defined State class
     Base.metadata.create_all(engine)
