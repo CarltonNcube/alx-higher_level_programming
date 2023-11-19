0588e59e2f5660afc667bf7e5d4f3e9188fd63a9
CarltonNcube <carltonncube@outlook.com>
2023-11-19 10:14:30 -0800

    a script that deletes all State objects with a name containing the letter a from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,35 +1,29 @@
 #!/usr/bin/python3
+""" 
+Prints the State object with the name passed as an argument from the database.
+Deletes all State objects whose name contains the letter 'a' and commits the changes.
 """
-Deletes all State objects with a name containing the letter 'a' from the database
-"""
+
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-def delete_states_with_a(username, password, database_name):
-    # Create a SQLAlchemy engine to connect to the MySQL server
-    engine = create_engine(f'mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}',
-            pool_pre_ping=True)
-
-    # Create a configured "Session" class
-    Session = sessionmaker(bind=engine)
-
-    # Create a Session
-    with Session() as session:
-        # Query and delete State objects with a name containing the letter 'a'
-        states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
-        for state in states_to_delete:
-            session.delete(state)
-
-        # Commit the changes to the database
-        session.commit()
-
 if __name__ == "__main__":
-    # Check if correct number of command-line arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
+    # Create a connection to the database engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    # Call the function with command-line arguments
-    delete_states_with_a(sys.argv[1], sys.argv[2], sys.argv[3])
+    # Create the tables in the database
+    Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query and delete State objects whose name contains the letter 'a'
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        session.delete(instance)
+
+    # Commit the changes to the database
+    session.commit()
