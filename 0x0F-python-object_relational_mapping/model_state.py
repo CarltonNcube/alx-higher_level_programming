fddf4140233c808c08f1414ed7b73753a9d79e05
CarltonNcube <carltonncube@outlook.com>
2023-11-19 07:57:38 -0800

    a python file that contains the class definition of a State and an instance Base = declarative_base()

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Define the State class and Base for database model
+Start link class to table in database 
 """
 
-from sqlalchemy import Column, Integer, String
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 # Declare the Base using declarative_base()
 Base = declarative_base()
@@ -12,13 +14,10 @@ Base = declarative_base()
 # Define the State class, inheriting from Base
 class State(Base):
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    # If this script is executed directly, create the table in the database
-    from sqlalchemy import create_engine
-
     # Create a SQLAlchemy engine to connect to the MySQL server
     # Use command-line arguments for MySQL username, password, and database name
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
@@ -27,3 +26,6 @@ if __name__ == "__main__":
     # Create the table in the MySQL database based on the defined State class
     Base.metadata.create_all(engine)
 
+    # Optionally, create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
