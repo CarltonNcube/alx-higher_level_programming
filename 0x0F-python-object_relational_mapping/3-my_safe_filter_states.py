a3f18e2eb278a9b6451f5d0cd5b5a2e49117945a
CarltonNcube <carltonncube@outlook.com>
2023-11-18 23:47:42 -0800

    a script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
 """
 Lists all states from the database hbtn_0e_0_usa.
-Safe from SQL injection.
 """
 
 import MySQLdb
 import sys
 
-# Connect to the MySQL database
-db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-        db=sys.argv[3], port=3306)
-cur = db.cursor()
+if __name__ == "__main__":
+    # Connect to the MySQL database
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    
+    # Create a cursor object to interact with the database
+    cur = db.cursor()
 
-# Use parameterized query to prevent SQL injection
-query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY id ASC"
-result = cur.execute(query, (sys.argv[4],))
+    # Get the state name to match from command-line arguments
+    match = sys.argv[4]
 
-# Check if the query was successful
-if result > 0:
+    # Use a parameterized query to prevent SQL injection
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match,))
+
     # Fetch all rows from the result set
     rows = cur.fetchall()
 
     # Print each row (state) in the result set
     for row in rows:
         print(row)
-else:
-    # Handle the case where the query was not successful
-    print("Error executing the query")
 
-# Close the cursor and database connection
-cur.close()
-db.close()
+    # Close the cursor and database connection
+    cur.close()
+    db.close()
