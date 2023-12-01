6ce96910d51785832b1a980df67027c92f4c65f7
CarltonNcube <carltonncube@outlook.com>
2023-12-01 08:18:27 -0800

    a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send a GET request and display the body for a 200 status code using curl
-curl -sL "$1" | [ "$(awk 'NR==1{print $2}')" -eq 200 ] && cat || echo "Error: Unexpected status code"
+curl -sI "$1" | grep "HTTP/1.1 200 OK" && curl -sL "$1" || echo "Error: Unexpected status code"
