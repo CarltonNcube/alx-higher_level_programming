85556b5372823dcb0d7530ac6c622b9282c7fc97
CarltonNcube <carltonncube@outlook.com>
2023-12-02 10:30:19 -0800

    a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     with request.urlopen(url, data=data) as response:
         body = response.read().decode('utf-8')
-        print("Your email is: {}".format(body))
+        print("{}".format(body))
