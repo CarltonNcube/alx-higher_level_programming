dd5804fe5033306d0f5aec1df82e836d03f184ae
CarltonNcube <carltonncube@outlook.com>
2023-12-02 11:09:45 -0800

    a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to send a GET request and display the body for a 200 status code using curl
-curl -s -w "%{http_code}" "$1" -o /dev/null | { read code; [ "$code" = "200" ] && curl -s "$1" || echo "Direct access"; }
+[ "$(curl -s -w "%{http_code}" "$1" | tail -c 3)" = "200" ] && curl -s "$1"
