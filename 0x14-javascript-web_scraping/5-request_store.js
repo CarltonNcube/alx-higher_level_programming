77c2021c94cac1e7fff07acbeb2ccfc30abcc1df
CarltonNcube <carltonncube@outlook.com>
2024-01-03 12:50:08 -0800

    a script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,8 +3,8 @@
 
 const request = require('request');
 const fs = require('fs');
-
-const [, , url, filePath] = process.argv;
+const url = process.argv[2];
+const file = process.argv[3];
 
 request(url, function (err, res, body) {
   if (err) {
