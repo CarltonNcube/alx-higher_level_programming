1ee4fb2cb016050ba20f577b1362db285d873636
CarltonNcube <carltonncube@outlook.com>
2024-01-03 12:37:46 -0800

    a script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,11 +7,11 @@ const [, , url, filePath] = process.argv;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.error(`Error fetching ${url}: ${error.message}`);
   } else {
     fs.writeFile(filePath, body, 'utf-8', (err) => {
       if (err) {
-        console.error(err);
+        console.error(`Error saving content to ${filePath}: ${err.message}`);
       } else {
         console.log(`The content has been saved to ${filePath}`);
       }
